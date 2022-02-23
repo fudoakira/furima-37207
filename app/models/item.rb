fa4959a813df88438e3df02161d494cb28a95381
fudoakira <fumi.032010@gmail.com>
2022-02-23 16:17:53 +0900

    価格範囲のバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
-  validates :item_name, :content, :price, presence: true
+  validates :item_name, presence: true
+  validates :content, presence: true
+  VALID_PRICE_REGEX = /\A[0-9]+\z/
+  validates :price, presence: true, numericality: { in: 300..9,999,999 }
+            format: { with: VALID_PRICE_REGEX,
+            message: 'must be entered in half-width numbers'}
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :item_quality_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_burden_id, numericality: { other_than: 1 , message: "can't be blank"}
