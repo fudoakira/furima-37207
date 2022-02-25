0546aa4f2d5f779a739663bdaeb20b66e74f4247
fudoakira <fumi.032010@gmail.com>
2022-02-25 20:43:43 +0900

    編集ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,10 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+  end
+  
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :content, :price, :category_id, :item_quality_id, :shipping_fee_burden_id, :prefecture_id, :days_to_ship_id).merge(user_id: current_user.id)
