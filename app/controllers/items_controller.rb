fa04ae6444425003b7cbd14d3c2862c0b10aef12
fudoakira <fumi.032010@gmail.com>
2022-02-25 22:08:55 +0900

    出品者以外へのリダイレクト実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -40,4 +43,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :content, :price, :category_id, :item_quality_id, :shipping_fee_burden_id, :prefecture_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
+
 end
