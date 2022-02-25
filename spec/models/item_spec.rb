87d0d874a10ae44ea5f686c95526cd5b9aa2d2a8
fudoakira <fumi.032010@gmail.com>
2022-02-25 12:02:43 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,6 +72,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
+      it 'userが紐づいていなければ保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
