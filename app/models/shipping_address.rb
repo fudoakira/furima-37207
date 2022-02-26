791b2b1653fcce9a3498a922c19295585afb8a59
fudoakira <fumi.032010@gmail.com>
2022-02-26 21:52:47 +0900

    バリデーション設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,12 @@
 class ShippingAddress < ApplicationRecord
+  belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  validates :post_code, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :municipalities, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
 end
