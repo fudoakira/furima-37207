a69ffff73da9bea6bc0ba80d4415f31e07af9777
fudoakira <fumi.032010@gmail.com>
2022-03-05 23:00:01 +0900

    payjpの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,19 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @order = Order.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
     if @order.valid?
+      Payjp.api_key = "sk_test_22bda75c30e27cc10fca3d36"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @order.save
       return redirect_to root_path
     else
@@ -16,5 +24,9 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order).permit(:post_code, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :content, :price, :category_id, :item_quality_id, :shipping_fee_burden_id, :prefecture_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
 end
