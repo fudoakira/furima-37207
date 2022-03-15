53782c084ecc867b93c919265b2c36129cb8555f
fudoakira <fumi.032010@gmail.com>
2022-03-15 22:31:27 +0900

    バリデーションなどの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,15 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @order = Order.new(order_params)
-    if @order.valid?
-      Payjp.api_key = "sk_test_22bda75c30e27cc10fca3d36"
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
-      @order.save
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      pay_item
+      @order_shipping_address.save
       return redirect_to root_path
     else
       render 'index'
@@ -23,10 +18,19 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:post_code, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.permit(:post_code, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def item_params
     params.require(:item).permit(:image, :item_name, :content, :price, :category_id, :item_quality_id, :shipping_fee_burden_id, :prefecture_id, :days_to_ship_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_22bda75c30e27cc10fca3d36"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -2,17 +2,15 @@ class OrderShippingAddress
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building, :phone_number, :user_id, :item_id, :token
 
-  with_options presence: true do
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "例）123-4567"}
-    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :municipalities
-    validates :address
-    validates :phone_number, format: {with: /\d{10,11}/}, length: {maximum: 11}
-    validates :user_id
-    validates :item_id
-    validates :token
-  end
-
+  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "must be entered as a half-width character string (example:123-4567)"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :municipalities, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, format: {with: /\d{10,11}/, message: "must be entered in 10-11 half-width numbers (example:09012345678)"},
+            length: {maximum: 11, message: "must be entered within 11 digits (example:09012345678)"}
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :token, presence: true
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building: building, phone_number: phone_number, order_id: order_id)
